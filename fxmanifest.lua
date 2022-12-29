bdd0d42f20b1139b4a357602a38ed5cb13f8348f
cadburry <archxcadburry@gmail.com>
2022-12-29 10:04:40 +0530

    version & checker

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,7 +2,7 @@ fx_version 'cerulean'
 game 'gta5'
 
 author 'Cadburry#7547'
-version '0.1'
+version '0.1.0'
 description 'NUI Vehicle Keys for QBCore Based Servers'
 
 shared_scripts {
---
+++ server/sv_customise.lua
@@ -0,0 +1,14 @@
+
+-- ██ ████████ ███████ ███    ███ ███████ 
+-- ██    ██    ██      ████  ████ ██      
+-- ██    ██    █████   ██ ████ ██ ███████ 
+-- ██    ██    ██      ██  ██  ██      ██ 
+-- ██    ██    ███████ ██      ██ ███████ 
+                                                                              
+QBCore.Functions.CreateUseableItem("lockpick", function(source, item)
+	TriggerClientEvent('lockpicks:UseLockpick', source, false)
+end)
+
+QBCore.Functions.CreateUseableItem("advancedlockpick", function(source, item)
+	TriggerClientEvent('lockpicks:UseLockpick', source, true)
+end)
